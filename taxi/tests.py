2c889f763fcddf559f6fe57eaf3a3cabdcbf8c90
Roman Smyk <smyk_roman@ukr.net>
2026-05-27 21:23:13 +0300

    Solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from taxi.models import Manufacturer, Car, Driver
+
+
+class CarSearchTests(TestCase):
+    def test_search_by_model(self):
+        manufacturer = Manufacturer.objects.create(name="BMW")
+        Car.objects.create(model="X5", manufacturer=manufacturer)
+        Car.objects.create(model="X3", manufacturer=manufacturer)
+        response = self.client.get(reverse("car-list"), {"model": "X5"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context["cars"]), 1)
+
+
+class DriverSearchTests(TestCase):
+    def test_search_by_username(self):
+        Driver.objects.create(username="john")
+        Driver.objects.create(username="mike")
+        response = self.client.get(reverse("driver-list"), {"username": "john"})
+        self.assertEqual(len(response.context["drivers"]), 1)
+
+
+class ManufacturerSearchTests(TestCase):
+    def test_search_by_name(self):
+        Manufacturer.objects.create(name="Tesla")
+        Manufacturer.objects.create(name="Toyota")
+        response = self.client.get(reverse("manufacturer-list"), {"name": "Tesla"})
+        self.assertEqual(len(response.context["manufacturers"]), 1)
--- taxi/views.py
+++ taxi/views.py
@@ -36,6 +36,13 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
+    def get_queryset(self):
+        queryset = Manufacturer.objects.all()
+        name = self.request.GET.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Manufacturer
@@ -58,6 +65,13 @@ class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
     queryset = Car.objects.select_related("manufacturer")
+
+    def get_queryset(self):
+        queryset = Car.objects.select_related("manufacturer")
+        model = self.request.GET.get("model")
+        if model:
+            queryset = queryset.filter(model__icontains=model)
+        return queryset
 
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
@@ -84,6 +98,13 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 5
+
+    def get_queryset(self):
+        queryset = Driver.objects.all()
+        username = self.request.GET.get("username")
+        if username:
+            queryset = queryset.filter(username__icontains=username)
+        return queryset
 
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
