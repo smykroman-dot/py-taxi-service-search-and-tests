ec7c62048b08967fce5957f363dcc46534f85093
Roman Smyk <smyk_roman@ukr.net>
2026-06-01 00:17:38 +0300

    Removed space % unused import

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,6 +1,4 @@
-from mailcap import test
-
-from django.contrib.auth import get_user, get_user_model
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
